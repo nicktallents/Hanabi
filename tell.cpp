e0b1cf512a80d4d18e841006bb6010eaf44d87c4
nicktallents <nicktallents11@gmail.com>
2014-10-26 18:35:14 -0400

    update KB...idk if it works yet

--- tell.cpp
+++ tell.cpp
@@ -14,48 +14,19 @@ void Player::tell(Event* e, vector<int> board, int hints, int fuses, vector<Card
 		// check if the play was legal and update proper vars
 		if (pe->legal) {
 			this->board = board;
-			// update KB
-			if (!pe->wasItThisPlayer) {
-				for (int i=0; i<KB.size(); i++) {
-					// if there's only one if the card left, then it's not discardable
-					if (remainingCount(KB[i].perceivedNum, KB[i].perceivedColor) == 1) {
-						KB[i].discardable = false;
-					}
-					// if it's the same color with same or lesser value
-					if (KB[i].perceivedColor == pe->c.color && KB[i].perceivedNum == pe->c.number) {
-						KB[i].usable = false;
-						KB[i].discardable = true;
-					}
-					// if it's usable, see if it's not usable / discardable
-					if (KB[i].usable) {
-						// if it's the same color, but we don't know the number...
-						if (KB[i].perceivedColor == pe->c.color && KB[i].perceivedNum == -1) {
-							KB[i].usable = false;
-						}
-						// if it's the same number, but we don't know the color...
-						else if (KB[i].perceivedColor == -1 && KB[i].perceivedNum == pe->c.number) {
-							KB[i].usable = false;
-						}
-					}
-					// if it's not usable, see if it's usable now
-					else {
-						if (KB[i].perceivedColor == pe->c.color && KB[i].perceivedNum - 1 == pe->c.number) {
-							KB[i].usable == true;
-						}
-					}
-				}
-			}
 		}
 		else {
 			this->discardPile.push_back(pe->c);
 			this->nFuses = fuses;
 			this->nHints = hints;
 		}
-		// remove the card from either the player's KB or partner's KB and hand
+		// remove the card from either the player's KB or partner's KB and hand and update KB / oKB
 		if (pe->wasItThisPlayer) {
+			updateKB(oKB, pe->c);
 			KB.erase(KB.begin()+pe->position);
 		}
 		else {
+			updateKB(KB, pe->c);
 			oKB.erase(oKB.begin()+pe->position);
 			this->oHand = oHand;
 		}
@@ -63,7 +34,35 @@ void Player::tell(Event* e, vector<int> board, int hints, int fuses, vector<Card
 	}
 }
 
-void updateKB(vector<CardInfo> &kb, Card c)
+void Player::updateKB(vector<CardInfo> &kb, Card c)
 {
-	;
+	// update kb
+	for (int i=0; i<kb.size(); i++) {
+		// if there's only one if the card left, then it's not discardable
+		if (remainingCount(kb[i].perceivedNum, kb[i].perceivedColor) == 1) {
+			kb[i].discardable = false;
+		}
+		// if it's the same color with same or lesser value
+		if (kb[i].perceivedColor == c.color && kb[i].perceivedNum == c.number) {
+			kb[i].usable = false;
+			kb[i].discardable = true;
+		}
+		// if it's usable, see if it's not usable / discardable
+		if (kb[i].usable) {
+			// if it's the same color, but we don't know the number...
+			if (kb[i].perceivedColor == c.color && kb[i].perceivedNum == -1) {
+				kb[i].usable = false;
+			}
+			// if it's the same number, but we don't know the color...
+			else if (kb[i].perceivedColor == -1 && kb[i].perceivedNum == c.number) {
+				kb[i].usable = false;
+			}
+		}
+		// if it's not usable, see if it's usable now
+		else {
+			if (kb[i].perceivedColor == c.color && kb[i].perceivedNum - 1 == c.number) {
+				kb[i].usable == true;
+			}
+		}
+	}
 }
